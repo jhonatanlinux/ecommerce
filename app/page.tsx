beda94ce4ff2b1592953ed87a87958f2b800c307
jhonatanlinux <jhonatan135135@gmail.com>
2023-11-30 13:52:07 -0400

    Next13 Fullstack E-Commerce 🛒 - 10 Banner | React.js & Typescript 🔥
    https://www.youtube.com/watch?v=qEU1dcA9p7Q&list=PL63c_Ws9ecISwPzddQzQ671S_gzJA6M5z&index=11

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/nav/HomeBanner";
+
 export default function Home() {
-  return <div>Loja Inovatech</div>;
+  return (
+    <div className="p-8"> 
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+      </Container>
+    </div>
+  );
 }
