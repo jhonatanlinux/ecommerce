9ecdf685d1c1dd3117eba490891d26801b4b4986
jhonatanlinux <jhonatan135135@gmail.com>
2023-12-01 15:35:45 -0400

    Next13 Fullstack E-Commerce 🛒 - 12 Product Card | React.js & Typescript 🔥
    https://www.youtube.com/watch?v=Qgup3sFcCiE&list=PL63c_Ws9ecISwPzddQzQ671S_gzJA6M5z&index=14

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/nav/HomeBanner";
 import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl-grid-cols-5 2xl:grid-cols-6 gap:8">
           {products.map((product: any) => {
-            return <div>{truncateText(product.name)}</div>;
+            return <ProductCard data={product} />;
           })}
         </div>
       </Container>
