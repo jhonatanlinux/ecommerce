ea6ce9c1e275a38b8f137383181af004d69d8cf5
jhonatanlinux <jhonatan135135@gmail.com>
2023-12-04 10:45:50 -0400

    Next13 Fullstack E-Commerce 🛒 - 14 Product Details Page | React.js & Typescript 🔥
    https://www.youtube.com/watch?v=It2SG-dBsS0&list=PL63c_Ws9ecISwPzddQzQ671S_gzJA6M5z&index=15

--- utils/products.tsx
+++ utils/products.tsx
@@ -1,183 +1,184 @@
 export const products = [
-    {
-      id: "64a654593e91b8e73a351e9b",
-      name: "Câmera Solar 4g Full Hd 1080p Roça Sítio Painel + Baterias",
-      description: "Short description",
-      price: 1999,
-      brand: "intelbras",
-      category: "Câmeras",
-      inStock: true,
-      images: [
-        {
-          color: "White",
-          colorCode: "#FFFFFF",
+  {
+    id: "64a654593e91b8e73a351e9b",
+    name: "Câmera Solar 4g Full Hd 1080p Roça Sítio Painel + Baterias",
+    description:
+      "Esse Kit tem 1 Câmera FULL HD, 360°, auto tracking (acompanha movimentação), áudio bidirecional, visão noturna de 20m com definição, durante o dia 50m com definição, zoom digital, memória interna para gravação. Esse kit grava em até 11 dias, tem memória interna (cartão de memória da câmera), e vai regravando sempre em cima da gravação mais antiga. A câmera desse kit é da marca Prime At (própria, importada), com vários recursos e com menor custo. Também, trabalhamos com as principais marcas do mercado de câmeras, Intelbras e Hikvison, e inclusive mais de uma câmera por kit, caso tenha necessidade, só avaliar os outros anúncios. Enviamos o equipamento já montato, configurado, testado em bancada e com os chicotes de instalações prontos. Garantia de todo kit de 12 meses direto conosco, somos fabricante. 24 meses de garantia na bateria, normalmente é na mesma cidade do cliente ou cidade mais próxima. Temos parcerias com várias marcas, Moura, Bosch, Heliar, Freedom, Pioneiro. Obs.: Comprar no máximo 2 unidades por pedido. Caso for mais que 2, só fazer mais pedidos, assim dá mais segurança no transporte.",
+    price: 1999,
+    brand: "intelbras",
+    category: "Câmeras",
+    inStock: true,
+    images: [
+      {
+        color: "White",
+        colorCode: "#FFFFFF",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2Fasdadasdasdadsasdas.png?alt=media&token=a94f8fe0-0479-458f-a9d2-7c363e9e1733",
+      },
+      {
+        color: "Gray",
+        colorCode: "#808080",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2Fasdasdasdasd.png?alt=media&token=3b4d2c06-fd27-4ccc-b335-2bb77ab62b63",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "64a4ebe300900d44bb50628a",
+    name: "Câmera Speed Dome Intelbras Vhd 5225 Sd Ir Starlight 1080p",
+    description:
+      "Câmera Speed Dome HDCVI Intelbras VHD 5225 SD IR. A câmera speed dome HDCVI da marca Intelbras VHD 5225 SD IR, apresenta alta resolução Full Hd e recursos inteligentes da série 5000 com alto poder de zoom óptico de 25x e infravermelho de alcance de 100 metros, possui sensibilidade noturna, com tecnologia Starlight. Esta câmera speed dome HDCVI VHD 5225 SD IR da marca Intelbras, acompanha fonte alimentada de corrente contínua de 12V, pode ser controlada através do cabo de vídeo, conta com IP66 promovendo proteção contra resíduos sólidos e líquidos.",
+    price: 3199.9,
+    brand: "intelbras",
+    category: "Câmeras",
+    inStock: true,
+    images: [
+      {
+        color: "Black",
+        colorCode: "#000000",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2FASDADASDFAadads.png?alt=media&token=b810f226-603b-4d07-8e2f-ecd2cb69eb94",
+      },
+    ],
+    reviews: [
+      {
+        id: "64a65a6158b470c6e06959ee",
+        userId: "6475af156bad4917456e6e1e",
+        productId: "64a4ebe300900d44bb50628a",
+        rating: 4,
+        comment: "Excelente Produto",
+        createdDate: "2023-12-01T06:08:33.067Z",
+        user: {
+          id: "6475af156bad4917456e6e1e",
+          name: "Jhonatan Alves",
+          email: "jhonatan135135@gmail.com",
+          emailVerified: null,
           image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2Fasdadasdasdadsasdas.png?alt=media&token=a94f8fe0-0479-458f-a9d2-7c363e9e1733",
+            "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
+          hashedPassword: null,
+          createdAt: "2023-12-01T08:08:53.979Z",
+          updatedAt: "2023-12-01T08:08:53.979Z",
+          role: "ADMIN",
         },
-        {
-          color: "Gray",
-          colorCode: "#808080",
+      },
+    ],
+  },
+  {
+    id: "648437b38c44d52b9542e340",
+    name: "Fechadura Elétrica Para Ambientes Residenciais Ou Comerciais",
+    description:
+      "A fechadura FX 2000 é a solução ideal para controle de acesso em ambientes comerciais ou residenciais.",
+    price: 40,
+    brand: "Apple",
+    category: "Phone",
+    inStock: true,
+    images: [
+      {
+        color: "Black",
+        colorCode: "#000000",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2F3A1DF3SD1Fsddsf.png?alt=media&token=0c4d77f3-2390-4b5d-85be-99b5935ffd12",
+      },
+      {
+        color: "Blue",
+        colorCode: " #0000FF",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2Fiphone%2012%20blue.png?alt=media&token=ede757d2-b631-4451-b80c-123861f16c92",
+      },
+      {
+        color: "Red",
+        colorCode: "#FF0000",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2Fiphone%2012%20red.png?alt=media&token=945e1ffb-953e-467a-8325-5a8fbbbf3153",
+      },
+    ],
+    reviews: [
+      {
+        id: "6499b4887402b0efd394d8f3",
+        userId: "6499b184b0e9a8c8709821d3",
+        productId: "648437b38c44d52b9542e340",
+        rating: 5,
+        comment:
+          "good enough. I like the camera and casing. the delivery was fast too.",
+        createdDate: "2023-06-26T15:53:44.483Z",
+        user: {
+          id: "6499b184b0e9a8c8709821d3",
+          name: "Chaoo",
+          email: "example1@gmail.com",
+          emailVerified: null,
           image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2Fasdasdasdasd.png?alt=media&token=3b4d2c06-fd27-4ccc-b335-2bb77ab62b63",
+            "https://lh3.googleusercontent.com/a/AAcHTtcuRLwWi1vPKaQOcJlUurlhRAIIq2LgYccE8p32=s96-c",
+          hashedPassword: null,
+          createdAt: "2023-06-26T15:40:52.558Z",
+          updatedAt: "2023-06-26T15:40:52.558Z",
+          role: "USER",
         },
-      ],
-      reviews: [],
-    },
-    {
-      id: "64a4ebe300900d44bb50628a",
-      name: "Câmera Speed Dome Intelbras Vhd 5225 Sd Ir Starlight 1080p",
-      description:
-        "Tem uma resolução Full HD 1080p pela qual você será capaz de gravar com a melhor qualidade e, assim, ter uma vigilância clara e precisa sobre seus espaços.",
-      price: 3199.90,
-      brand: "intelbras",
-      category: "Câmeras",
-      inStock: true,
-      images: [
-        {
-          color: "Black",
-          colorCode: "#000000",
+      },
+      {
+        id: "6499a110efe4e4de451c7edc",
+        userId: "6475af156bad4917456e6e1e",
+        productId: "648437b38c44d52b9542e340",
+        rating: 3,
+        comment: "I really liked it!!",
+        createdDate: "2023-06-26T14:30:40.998Z",
+        user: {
+          id: "6475af156bad4917456e6e1e",
+          name: "Charles",
+          email: "example@gmail.com",
+          emailVerified: null,
           image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2FASDADASDFAadads.png?alt=media&token=b810f226-603b-4d07-8e2f-ecd2cb69eb94",
+            "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
+          hashedPassword: null,
+          createdAt: "2023-05-30T08:08:53.979Z",
+          updatedAt: "2023-05-30T08:08:53.979Z",
+          role: "ADMIN",
         },
-      ],
-      reviews: [
-        {
-          id: "64a65a6158b470c6e06959ee",
-          userId: "6475af156bad4917456e6e1e",
-          productId: "64a4ebe300900d44bb50628a",
-          rating: 4,
-          comment: "Excelente Produto",
-          createdDate: "2023-12-01T06:08:33.067Z",
-          user: {
-            id: "6475af156bad4917456e6e1e",
-            name: "Jhonatan Alves",
-            email: "jhonatan135135@gmail.com",
-            emailVerified: null,
-            image:
-              "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
-            hashedPassword: null,
-            createdAt: "2023-12-01T08:08:53.979Z",
-            updatedAt: "2023-12-01T08:08:53.979Z",
-            role: "ADMIN",
-          },
-        },
-      ],
-    },
-    {
-      id: "648437b38c44d52b9542e340",
-      name: "Fechadura Elétrica Para Ambientes Residenciais Ou Comerciais",
-      description:
-        'A fechadura FX 2000 é a solução ideal para controle de acesso em ambientes comerciais ou residenciais.',
-      price: 40,
-      brand: "Apple",
-      category: "Phone",
-      inStock: true,
-      images: [
-        {
-          color: "Black",
-          colorCode: "#000000",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2F3A1DF3SD1Fsddsf.png?alt=media&token=0c4d77f3-2390-4b5d-85be-99b5935ffd12",
-        },
-        {
-          color: "Blue",
-          colorCode: " #0000FF",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2Fiphone%2012%20blue.png?alt=media&token=ede757d2-b631-4451-b80c-123861f16c92",
-        },
-        {
-          color: "Red",
-          colorCode: "#FF0000",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2Fiphone%2012%20red.png?alt=media&token=945e1ffb-953e-467a-8325-5a8fbbbf3153",
-        },
-      ],
-      reviews: [
-        {
-          id: "6499b4887402b0efd394d8f3",
-          userId: "6499b184b0e9a8c8709821d3",
-          productId: "648437b38c44d52b9542e340",
-          rating: 5,
-          comment:
-            "good enough. I like the camera and casing. the delivery was fast too.",
-          createdDate: "2023-06-26T15:53:44.483Z",
-          user: {
-            id: "6499b184b0e9a8c8709821d3",
-            name: "Chaoo",
-            email: "example1@gmail.com",
-            emailVerified: null,
-            image:
-              "https://lh3.googleusercontent.com/a/AAcHTtcuRLwWi1vPKaQOcJlUurlhRAIIq2LgYccE8p32=s96-c",
-            hashedPassword: null,
-            createdAt: "2023-06-26T15:40:52.558Z",
-            updatedAt: "2023-06-26T15:40:52.558Z",
-            role: "USER",
-          },
-        },
-        {
-          id: "6499a110efe4e4de451c7edc",
-          userId: "6475af156bad4917456e6e1e",
-          productId: "648437b38c44d52b9542e340",
-          rating: 3,
-          comment: "I really liked it!!",
-          createdDate: "2023-06-26T14:30:40.998Z",
-          user: {
-            id: "6475af156bad4917456e6e1e",
-            name: "Charles",
-            email: "example@gmail.com",
-            emailVerified: null,
-            image:
-              "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
-            hashedPassword: null,
-            createdAt: "2023-05-30T08:08:53.979Z",
-            updatedAt: "2023-05-30T08:08:53.979Z",
-            role: "ADMIN",
-          },
-        },
-      ],
-    },
-    {
-      id: "64a4e9e77e7299078334019f",
-      name: "Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.",
-      description:
-        "Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.",
-      price: 80,
-      brand: "intelbras",
-      category: "Acessórios",
-      inStock: true,
-      images: [
-        {
-          color: "Graphite",
-          colorCode: " #383838",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2F32295d1f214ba9ea7f05338da9cf9b11834f9fd77505ca50de0e29ee8026d000.png?alt=media&token=1291b4cc-173f-44fc-ac1e-1afe560675b9",
-        },
-      ],
-      reviews: [],
-    },
-    {
-      id: "649d775128b6744f0f497040",
-      name: 'Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.',
-      description:
-        'Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.',
-      price: 50,
-      brand: "intelbras",
-      category: "Acessórios",
-      inStock: true,
-      images: [
-        {
-          color: "Black",
-          colorCode: "#000000",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2FRASTRO-VENDA-INSTALACAO-DE-CERCA-ELETRICA-CERCA-CONCERTINA-03.png?alt=media&token=69df74a4-c6b8-40a6-92dc-1e70b5b4a211",
-        },
-        {
-          color: "Silver",
-          colorCode: "#C0C0C0",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2FRASTRO-VENDA-INSTALACAO-DE-CERCA-ELETRICA-CERCA-CONCERTINA-03.png?alt=media&token=69df74a4-c6b8-40a6-92dc-1e70b5b4a211",
-        },
-      ],
-      reviews: [],
-    },
-  ];
+      },
+    ],
+  },
+  {
+    id: "64a4e9e77e7299078334019f",
+    name: "Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.",
+    description:
+      "Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.",
+    price: 80,
+    brand: "intelbras",
+    category: "Acessórios",
+    inStock: true,
+    images: [
+      {
+        color: "Graphite",
+        colorCode: " #383838",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2F32295d1f214ba9ea7f05338da9cf9b11834f9fd77505ca50de0e29ee8026d000.png?alt=media&token=1291b4cc-173f-44fc-ac1e-1afe560675b9",
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: "649d775128b6744f0f497040",
+    name: "Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.",
+    description:
+      "Instalação e manutenção de cercas elétricas, concertinas e sistemas de segurança.",
+    price: 50,
+    brand: "intelbras",
+    category: "Acessórios",
+    inStock: true,
+    images: [
+      {
+        color: "Black",
+        colorCode: "#000000",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2FRASTRO-VENDA-INSTALACAO-DE-CERCA-ELETRICA-CERCA-CONCERTINA-03.png?alt=media&token=69df74a4-c6b8-40a6-92dc-1e70b5b4a211",
+      },
+      {
+        color: "Silver",
+        colorCode: "#C0C0C0",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/ecommerce-55dbc.appspot.com/o/products%2FRASTRO-VENDA-INSTALACAO-DE-CERCA-ELETRICA-CERCA-CONCERTINA-03.png?alt=media&token=69df74a4-c6b8-40a6-92dc-1e70b5b4a211",
+      },
+    ],
+    reviews: [],
+  },
+];
