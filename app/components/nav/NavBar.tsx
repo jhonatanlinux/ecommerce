762598c97dbd61b99a57162a49e94cd42a85f101
jhonatanlinux <jhonatan135135@gmail.com>
2023-12-05 16:04:45 -0400

    LOGO MARCA
    LOGO MARCA

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -10,11 +10,12 @@ const NavBar = () => {
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex items-center justify-between gap-3 md:gap-0">
-            <Link
-              href="/"
-              className={`${redressed.className} font-bold text-2xl`}
-            >
-              Inovatech
+            <Link href="/">
+              <img
+                src="/logo.png"
+                alt="Inovatech Logo"
+                className="h-10 cursor-pointer"
+              />
             </Link>
             <div className="hidden md:block">Buscar</div>
             <div className="flex items-center gap-8 md:gap-12">
