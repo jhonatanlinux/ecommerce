df442cb46f745e7c994dfd6c9179d65b7fa2e682
jhonatanlinux <jhonatan135135@gmail.com>
2023-12-06 13:41:08 -0400

    Next13 Fullstack E-Commerce 🛒 - 21 Avatar Component | React.js & Typescript 🔥
    https://www.youtube.com/watch?v=jGyW6Sw-4jU&list=PL63c_Ws9ecISwPzddQzQ671S_gzJA6M5z&index=23

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import Avatar from "@/app/components/products/Avatar";
 import { Rating } from "@mui/material";
 import moment from "moment";
 
@@ -19,7 +20,7 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
             return (
               <div key={review.id} className="max-w-300px">
                 <div className="flex gap-2 items-center">
-                  <div>Avatar</div>
+                  <Avatar src={review?.user.image}/>
                   <div className="font-semibold">{review?.user.name}</div>
                   <div className="font-light">
                     {moment(review.createdDate).fromNow()}
