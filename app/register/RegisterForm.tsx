2025fc20d59a383fe4442d342e09922921add0b9
Jhonatan Alves <45457692+jhonatanlinux@users.noreply.github.com>
2023-12-19 23:58:02 -0400

    Next13 Fullstack E-Commerce 🛒- 36 Authentication in Next13, NextAuth, Prisma and MongoDB 🙌
    https://youtu.be/vZ2mjwvcfSo?list=PL63c_Ws9ecISwPzddQzQ671S_gzJA6M5z&t=4249

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,10 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,9 +26,35 @@ const RegisterForm = () => {
     },
   });
 
+  const router = useRouter();
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-    console.log(data);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Conta Criada!");
+
+        signIn("credential", {
+          email: data.email,
+          password: data.passwor,
+          redirect: false,
+        }).then((callback) => {
+          if (callback?.ok) {
+            router.push("/cart");
+            router.refresh();
+            toast.success("Você esta Logado!");
+          }
+          if (callback?.error) {
+            toast.error(callback.error);
+          }
+        });
+      })
+      .catch(() => toast.error("Tente mais tarde!"))
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
